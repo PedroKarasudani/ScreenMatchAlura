fadc09b9d07989e2f76fa40e4ee972f48a9f83ae
Pedro K. L <pedro.karasudani@hotmail.com>
2023-05-10 22:24:06 -0300

    iniciando

---
+++ src/screenmatchalura/Filme.java
@@ -0,0 +1,13 @@
+
+package screenmatchalura;
+
+
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluindoNoPlano;
+    double avaliacao;
+    int totalDeAvalicoes; 
+    
+    
+}
---
+++ src/screenmatchalura/ScreenMatchAlura.java
@@ -0,0 +1,11 @@
+
+package screenmatchalura;
+
+public class ScreenMatchAlura {
+
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
