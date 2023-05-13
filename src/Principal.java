e83ee4ca126aba26574c739e044c1217bbbca2af
Pedro K. L <pedro.karasudani@hotmail.com>
2023-05-13 17:17:33 -0300

    Herança
    Criamos a classe titulo, para ter a heranca com a classe Filme e Serie..

--- src/Principal.java
+++ src/Principal.java
@@ -1,18 +1,20 @@
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
 
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.setAnoDeLancamento(1970);
-        meuFilme.setNome("Top Gun");
-
-        meuFilme.exibeFichaTecnica();
-        meuFilme.avalia(8);
-        meuFilme.avalia(5);
-        meuFilme.avalia(10);
-        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
-        System.out.println(meuFilme.pegaMedia());
+        meuFilme.setNome("Veloses Furiosos");
+        meuFilme.setDiretor("Vin Diesel");
+        System.out.println(meuFilme.getNome());
+        System.out.println(meuFilme.getDiretor());
+        
+        Serie minhaSerie = new Serie();
+        minhaSerie.setNome("La Casa De Papel");
+        minhaSerie.setTemporadas(5);
+        System.out.println(minhaSerie.getNome());
+        System.out.println(minhaSerie.getTemporadas());
 
     }
 
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,65 +1,13 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
+public class Filme extends Titulo{
+    private String diretor;
 
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluindoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacoes;
-    private int duracaoEmMinutos;
-
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public void setIncluindoNoPlano(boolean incluindoNoPlano) {
-        this.incluindoNoPlano = incluindoNoPlano;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
-
-    public boolean isIncluindoNoPlano() {
-        return incluindoNoPlano;
-    }
-
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-
-    public double getSomaDasAvaliacoes() {
-        return somaDasAvaliacoes;
-    }
-
-    public int getTotalDeAvaliacoes() {
-        return totalDeAvaliacoes;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void exibeFichaTecnica() {
-        System.out.println("Nome do Filme: " + this.getNome());
-        System.out.println("Ano de Lancamento: " + this.getAnoDeLancamento());
-    }
-
-    public void avalia(double nota) {
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-
-    public double pegaMedia() {
-        return this.getTotalDeAvaliacoes() / this.getSomaDasAvaliacoes();
+    public String getDiretor() {
+        return diretor;
     }
 }
