f9503ef499b011cc88c77a274be995bc90f47eda
Pedro K. L <pedro.karasudani@hotmail.com>
2023-05-12 22:02:52 -0300

    criou pacotes, filme separando do projeto principal
    implementou os gets e sets, e modo de visualização das variáveis

--- src/Principal.java
+++ src/Principal.java
@@ -1,18 +1,17 @@
-package screenmatchalura;
+import br.com.alura.screenmatch.modelos.Filme;
 
 public class Principal {
 
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.nome = "Top Gun";
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setNome("Top Gun");
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
 
     }
