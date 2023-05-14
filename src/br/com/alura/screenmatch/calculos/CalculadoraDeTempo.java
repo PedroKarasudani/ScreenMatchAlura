5a2841b9fe2301e35c61468df0c54f1162e1712e
Pedro K. L <pedro.karasudani@hotmail.com>
2023-05-13 21:29:50 -0300

    Polimorfismo
    modifiquei a classe CalculadoraDeTempo, com isso existindo somente um inclui

--- src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
+++ src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
@@ -1,20 +1,24 @@
 package br.com.alura.screenmatch.calculos;
 
-import br.com.alura.screenmatch.modelos.Filme;
-import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
 
 public class CalculadoraDeTempo {
 
     private int tempoTotal;
 
-    public void inclui(Filme filme) {
-        tempoTotal += filme.getDuracaoEmMinutos();
-    }
+//    public void inclui(Filme filme) {
+//        tempoTotal += filme.getDuracaoEmMinutos();
+//    }
+//
+//    public void inclui(Serie serie) {
+//        tempoTotal += serie.getDuracaoEmMinutos();
+//    }
 
-    public void inclui(Serie serie) {
-        tempoTotal += serie.getDuracaoEmMinutos();
+    public void inclui(Titulo titulo){
+        tempoTotal += titulo.getDuracaoEmMinutos();
     }
-
+    
     public int getTempoTotal() {
         return this.tempoTotal;
     }
