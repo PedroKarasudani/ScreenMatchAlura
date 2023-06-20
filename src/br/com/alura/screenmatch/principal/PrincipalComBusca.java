a4c15764dca4c36afb75fdb710b81b531999d9c1
Pedro K. L <pedro.karasudani@hotmail.com>
2023-06-19 21:16:56 -0300

    TRY-CATCH - múltiplas exceções separadas

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -6,6 +6,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.IllegalFormatException;
 import java.util.Scanner;
 
 import com.google.gson.FieldNamingPolicy;
@@ -17,36 +18,37 @@ import br.com.alura.screenmatch.modelos.TituloComIMDB;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Scanner leitura = new Scanner(System.in);
-        System.out.print("Digite um filme: ");
-        var busca = leitura.nextLine();
-
-        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=f94aab4b";
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, BodyHandlers.ofString());
-
-        String json = response.body();
-        System.out.println(json);
-
-        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-        TituloComIMDB tituloComIMDB = gson.fromJson(json, TituloComIMDB.class);
-        // System.out.println(tituloComIMDB);
-
         try {
+            Scanner leitura = new Scanner(System.in);
+            System.out.print("Digite um filme: ");
+            var busca = leitura.nextLine();
+
+            String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=f94aab4b";
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endereco))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, BodyHandlers.ofString());
+
+            String json = response.body();
+            System.out.println(json);
+
+            Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+            TituloComIMDB tituloComIMDB = gson.fromJson(json, TituloComIMDB.class);
+            // System.out.println(tituloComIMDB);
+
             Titulo meuTitulo = new Titulo(tituloComIMDB);
             System.out.println(meuTitulo);
-        } catch (NumberFormatException | NullPointerException e) {
+        } catch (NumberFormatException e) {
             System.out.print("Deu um erro em: ");
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro!! verificar a sintax");
             System.out.println(e.getMessage());
         } finally {
             System.out.println("Finalizou");
         }
-
     }
-
 }
